6e1526bf19268c4e41b7982acec2655a407d8c36
Savinay <snangali@purdue.edu>
2015-04-14 15:29:08 -0400

    struggling maps

---
+++ src/com/example/LaziestBoy/MapTestActivity.java
@@ -0,0 +1,18 @@
+package com.example.LaziestBoy;
+
+/**
+ * Created by Sav on 4/13/15.
+ */
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MapTestActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.map_test);
+    }
+}
