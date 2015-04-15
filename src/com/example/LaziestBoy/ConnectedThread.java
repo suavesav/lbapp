cd12bb98318865fa46f7a2b53449274237c85e07
Savinay <snangali@purdue.edu>
2015-04-15 00:41:58 -0400

    Should be ready for testing with the microcontroller

---
+++ src/com/example/LaziestBoy/ConnectedThread.java
@@ -0,0 +1,65 @@
+package com.example.LaziestBoy;
+
+import android.bluetooth.BluetoothSocket;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import android.os.Handler;
+
+class ConnectedThread extends Thread {
+    private final BluetoothSocket mmSocket;
+    private final InputStream mmInStream;
+    private final OutputStream mmOutStream;
+    Handler mHandler;
+    final static int MESSAGE_READ = 1;
+
+    public ConnectedThread(BluetoothSocket socket) {
+        mmSocket = socket;
+        InputStream tmpIn = null;
+        OutputStream tmpOut = null;
+
+        // Get the input and output streams, using temp objects because
+        // member streams are final
+        try {
+            tmpIn = socket.getInputStream();
+            tmpOut = socket.getOutputStream();
+        } catch (IOException e) { }
+
+        mmInStream = tmpIn;
+        mmOutStream = tmpOut;
+    }
+
+    public void run() {
+        byte[] buffer;  // buffer store for the stream
+        int bytes; // bytes returned from read()
+
+        // Keep listening to the InputStream until an exception occurs
+        while (true) {
+            try {
+                buffer = new byte[1024];
+                // Read from the InputStream
+                bytes = mmInStream.read(buffer);
+                // Send the obtained bytes to the UI activity
+                mHandler.obtainMessage(MESSAGE_READ, bytes, -1, buffer)
+                        .sendToTarget();
+            } catch (IOException e) {
+                break;
+            }
+        }
+    }
+
+    /* Call this from the main activity to send data to the remote device */
+    public void write(byte[] bytes) {
+        try {
+            mmOutStream.write(bytes);
+        } catch (IOException e) { }
+    }
+
+    /* Call this from the main activity to shutdown the connection */
+    public void cancel() {
+        try {
+            mmSocket.close();
+        } catch (IOException e) { }
+    }
+}
--- src/com/example/LaziestBoy/TourListActivity.java
+++ src/com/example/LaziestBoy/TourListActivity.java
@@ -1,29 +1,87 @@
 package com.example.LaziestBoy;
 
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.os.Bundle;
 import android.app.ListActivity;
+import android.os.Handler;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 import android.widget.ListView;
 import android.view.View;
+import android.widget.Toast;
+import android.os.Message;
+
+import java.util.List;
+import java.util.Set;
 
 public class TourListActivity extends ListActivity {
     private TextView selection;
     private static final String[] items = {"Official Campus Tour", "Sports Tour", "Dorms Tour", "Select Destination"};
-    /**
-     * Called when the activity is first created.
-     */
+//    private List<BluetoothDevice> mPaired;
+    private BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+    private static final int SUCCESS_CONNECT = 0;
+    private static final int MESSAGE_READ = 1;
+    static Handler mHandler;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tourlist);
+
+//        mPaired = getIntent().getExtras().getParcelableArrayList("device.list");
+        String deviceName = "LAZIESTBOY";
+//
+        BluetoothDevice result = null;
+
+        Set<BluetoothDevice> devices = mBluetoothAdapter.getBondedDevices();
+        if (devices != null) {
+            for (BluetoothDevice device : devices) {
+//                showToast(result.getUuids()[0].getUuid().toString());
+                if (deviceName.equals(device.getName())) {
+                    result = device;
+                    break;
+                }
+            }
+        }
+
+        mHandler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+                switch (msg.what){
+                    case SUCCESS_CONNECT:
+                        showToast("Connected to device");
+//                        String s = "Application Checking in!";
+                        break;
+                    case MESSAGE_READ:
+                        showToast((String)msg.obj);
+                        break;
+                }
+            }
+        };
+
+        ConnectThread cThread = new ConnectThread(result);
+        BluetoothSocket mSocket = cThread.getSocket();
+        cThread.run();
+
+        ConnectedThread connectedThread = new ConnectedThread(mSocket);
+        connectedThread.run();
+
         setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items));
         selection = (TextView)findViewById(R.id.selection);
+
+
     }
     public void onListItemClick(ListView parent, View v, int position, long id)
     {
         selection.setText(items[position]);
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
     }
 
 }
